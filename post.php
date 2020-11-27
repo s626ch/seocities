b6885062c55aeccb15705980666bc28f1f1fecc1
Kirk Johnson <hendicted@funshitposting.xyz>
2020-11-27 20:26:39 +0100

    spam protection

--- post.php
+++ post.php
@@ -2,8 +2,11 @@
 include "config.php";
 $postName = htmlspecialchars($_POST["name"]);
 $postText = htmlspecialchars($_POST["post"]);
+$title = strlen($postName);
 $sites = fopen('sites.html', 'a');
-fwrite($sites, "<a href='$address/$postName'>$address/$postName</a><br>");
+if ($title > 21) exit("Your site name cannot be longer than 20 characters.");
+if ($postName = null || $postText = null) exit("You forgot to input on one of the fields.");
+fwrite($sites, "<li><a href='$address/$postName'>$postName</a></li><br>");
 mkdir($postName);
 chdir($postName);
 file_put_contents("index.html", $postText);
