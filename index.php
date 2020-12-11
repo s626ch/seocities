f1eb5603d8290875f75d46ae042382c492ae2964
Kirk Johnson <hendicted@funshitposting.xyz>
2020-12-11 12:56:58 +0100

    why did i use htmlspecialchars?

--- index.php
+++ index.php
@@ -3,18 +3,20 @@
 <script src="https://codemirror.net/mode/javascript/javascript.js"></script>
 <h1>welcome to seocities!</h1>
 <div id="postBox">
-<form action="post.php" method="post">
-<table>
-<tr>
-<td>Name:</td><td> <input type="text" name="name" value="Anonymous"><input type="submit" value="Post"></td>
-</tr>
-<tr>
-<td>Code:</td><td><textarea name="post"></textarea></td>
-</tr>
-</table>
-</form>
+    <form action="post.php" method="post">
+        <table>
+            <tr>
+                <td>Name:</td>
+                <td> <input type="text" name="name" value="Anonymous"><input type="submit" value="Post"></td>
+            </tr>
+            <tr>
+                <td>Code:</td>
+                <td><textarea name="post"></textarea></td>
+            </tr>
+        </table>
+    </form>
 </div>
-<h1>List of Websites</h1>
+<p>List of Websites</p>
 <!--database fetch here, WIP-->
 <?php
 include "config.php";
--- post.php
+++ post.php
@@ -1,11 +1,11 @@
 <?php
 include "config.php";
-$postName = htmlspecialchars($_POST["name"]);
-$postText = htmlspecialchars($_POST["post"]);
+$postName = $_POST["name"];
+$postText = $_POST["post"];
 $title = strlen($postName);
 $sites = fopen('sites.html', 'a');
-if ($title > 21) exit("Your site name cannot be longer than 20 characters.");
-if ($postName = null || $postText = null) exit("You forgot to input on one of the fields.");
+if ($title > 20) exit("Your site name cannot be longer than 20 characters.");
+if ($postName == null || $postText == null) exit("You forgot to input on one of the fields.");
 fwrite($sites, "<li><a href='$address/$postName'>$postName</a></li><br>");
 mkdir($postName);
 chdir($postName);
